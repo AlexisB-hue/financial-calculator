c92f9dfc9034b4a5ec72c62fbb2ff2b508fb0a82
Alexis Barrett <happynessb09@gmail.com>
2026-04-15 15:41:48 -0400

    Setting up to prompt the user

---
+++ src/main/java/com/pluralsight/calculator2.java
@@ -0,0 +1,15 @@
+package com.pluralsight;
+
+public class calculator2 {
+    public static void main(String[] args){
+        // prompting the user.
+        System.out.print("How much would you like to deposit? ");
+        System.out.print("What is its interest rate? ");
+        System.out.print("How many years would you keep it in this CD? ");
+
+        // computing the information.
+
+
+        //displaying our work.
+    }
+}
